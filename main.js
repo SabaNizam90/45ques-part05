80407c305d2af129deec5d2fbd48d270878e5d04
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-07-09 23:44:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Louis L'Amour";
+var Quote = "Start writing,no matter what.The water does not flow until the faucet is turned on.";
+console.log("".concat(famous_person, " ONce Said, ").concat(Quote));
---
+++ main.ts
@@ -0,0 +1,11 @@
+//5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+
+let famous_person =`Louis L'Amour`
+let Quote = "Start writing,no matter what.The water does not flow until the faucet is turned on."
+
+console.log(`${famous_person} ONce Said, ${Quote}`)
+
+
+
+
